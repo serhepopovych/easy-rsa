serhepopovych/easy-rsa

commit 5ee339e75d52b72faedaf4f4786ffd0f8dec39c0

easy-rsa: Skip any commands for uninitialized CA except build-ca
User might provide ~/easy-rsa/vars-<name>CA but not yet initialized CA
thus any command, except build-ca or build-inter which is a symlink to
build-ca will fail.

Skip all, but build-ca/build-inter commands on uninitialized CA and give
informative message to user.

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

easy-rsa/easy-rsa-lib.sh
@@ -191,3 +191,12 @@ umask $(printf -- '%04o\n' $(($(umask) | 0022))) ||:
 Make sure you are running via \"exec-ca <ca> ...\"
 with <ca> pointing to valid \"vars-<ca>\" file.
 "
+
+# Check for initialized CA or build-ca (build-inter symlink to build-ca)
+[ "$this" -ef "$EASY_RSA/build-ca" -o \
+  ! -d "$KEY_DIR" -o -s "$KEY_DIR/index.txt" ] ||
+    abort "$this_prog: no \"$KEY_DIR/index.txt\": CA is not initialized
+
+Make sure you initialized it with build-ca or build-inter
+commands before use and thus \"$KEY_DIR/index.txt\" is not empty.
+"

commit 43e7198e23c1d7a3bcab75dc72060548ecf20457

easy-rsa: Use serial file to test for initialized CA
Otherwise any easy-rsa command will fail even on initialized, but not
yet issued any certificates CA.

Fixes: 5ee339e75d52 ("easy-rsa: Skip any commands for uninitialized CA except build-ca")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

easy-rsa/easy-rsa-lib.sh
@@ -195,9 +195,9 @@ with <ca> pointing to valid \"vars-<ca>\" file.
 # Check for initialized CA or build-ca (build-inter symlink to build-ca)
 [ "$this" -ef "$EASY_RSA/build-ca"  -o \
   "$this" -ef "$EASY_RSA/clean-all" -o \
-  ! -d "$KEY_DIR" -o -s "$KEY_DIR/index.txt" ] ||
-    abort "$this_prog: no \"$KEY_DIR/index.txt\": CA is not initialized
+  ! -d "$KEY_DIR" -o -s "$KEY_DIR/serial" ] ||
+    abort "$this_prog: no \"$KEY_DIR/serial\": CA is not initialized
 
 Make sure you initialized it with build-ca or build-inter
-commands before use and thus \"$KEY_DIR/index.txt\" is not empty.
+commands before use and thus \"$KEY_DIR/serial\" is not empty.
 "