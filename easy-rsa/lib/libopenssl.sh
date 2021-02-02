serhepopovych/easy-rsa

commit 3957b601244580c98295ce1158293fbdec258feb

easy-rsa: Explicitly set oneline format for openssl x509 output
Default output varies from one openssl(1) version to another unless
explicitly specified with -nameout. Explicitly use oneline with
additional modifiers to ensure output does not depend on openssl(1)
version. Add use_quote nameopt explicitly to quote value when it is
applicable according to rfc2253.

It seems that index.txt database format is fixed and uses '/' to
separate fields which is now different from one returned by oneline
nameopt. Parse it directly.

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

easy-rsa/lib/libopenssl.sh
@@ -269,7 +269,12 @@ ossl_x509_get_var()
 
 	local var
 
-	var="$(ossl_x509 "$crt" "$@" -noout)" || return
+	var="$(
+	    ossl_x509 "$crt" "$@" \
+	        -noout \
+	        -nameopt oneline,-space_eq,sep_comma_plus,use_quote \
+	        #
+	)" || return
 
 	echo "$var" |
 	sed -n -e "s/^\(${var_regex}\)\s*=\s*\(.\+\)\s*\$/${var_name}='\\$n'/p"
@@ -283,7 +288,16 @@ ossl_get_field4dn_by_name()
 	local dn="${1:?missing 1st arg to ${func}() <dn>}"
 	local fn="${2:?missing 2d arg to ${func}() <fn>}"
 
-	echo "$dn" | sed -n -e "s/^.*\/\($fn\+\)=\([^/]\+\).*\$/'\2'/p"
+	local t=",$dn,"
+
+	t="${t##*,$fn=}" && [ "$t" != "$dn" ] || return
+	if [ -n "${t##\"*}" ]; then
+		t="${t%%,*}"
+	else
+		t="${t#\"}" && t="${t%%\",*}"
+	fi
+
+	[ -n "$t" ] && echo "$t" || return
 }
 
 # Usage: ossl_date2ts [<ossl_date>]
@@ -576,7 +590,15 @@ ossl_index_txt_same_pubkey_flist()
 		local pem="$(ossl_index_txt_filename "$7" "$8")"
 
 		if ! (V=0 valid_file "$pem"); then
-			pem="$(ossl_get_field4dn_by_name "$9" 'CN').crt"
+			pem="$(
+			    IFS='/'
+			    for t in $9; do
+			        if [ "${t%%=*}" = 'CN' ]; then
+			            echo "${t#CN=}"
+			            break
+			        fi
+			    done
+			).crt"
 			if ! (V=0 valid_file "$pem"); then
 				return 0
 			fi
@@ -667,8 +689,7 @@ ossl_index_txt_revoke_certs()
 		# issued certificates with the same CN will never reuse
 		# possibly compromised private key and we have a copy.
 
-		CN="$(ossl_get_field4dn_by_name "$subject" 'CN')"
-		if [ -n "$CN" ]; then
+		if CN="$(ossl_get_field4dn_by_name "$subject" 'CN')"; then
 			mv -f "$CN.crt" "$CN.crt.$serial.revoked" ||:
 			mv -f "$CN.csr" "$CN.csr.$serial.revoked" ||:
 			mv -f "$CN.key" "$CN.key.$serial.revoked" ||:

commit b29bae797300790ea3acf29e318cf632f297b479

easy-rsa: Unify x509 DN to fix ossl_get_field4dn_by_name() API breakage
Since DN format in index.txt differs from improved oneline DN format as
returned by ossl_x509_get_var() and ossl_get_field4dn_by_name() modifed
to support only improved format, index.txt DN format needs to be parsed
explicitly.

However there are too many plases besides library itself where that DN
is parsed using ossl_get_field4dn_by_name() that is correct API for
this task. Modifying such places to parse field (e.g. CN) explicitly
does not seem to be suitable.

Instead modify ossl_index_txt_line2args() to make it return DN in
improved oneline format compatible with ossl_get_field4dn_by_name().

Fixes: 3957b6012445 ("easy-rsa: Explicitly set oneline format for openssl x509 output")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

easy-rsa/lib/libopenssl.sh
@@ -476,7 +476,7 @@ ossl_index_txt_line2args()
 	filename="$5"
 
 	# DN
-	dn="$6"
+	dn="$(IFS='/' && set -- ${6#/} && IFS=',' && echo "$*")"
 
 	line="\
 		'$line' \
@@ -590,15 +590,7 @@ ossl_index_txt_same_pubkey_flist()
 		local pem="$(ossl_index_txt_filename "$7" "$8")"
 
 		if ! (V=0 valid_file "$pem"); then
-			pem="$(
-			    IFS='/'
-			    for t in $9; do
-			        if [ "${t%%=*}" = 'CN' ]; then
-			            echo "${t#CN=}"
-			            break
-			        fi
-			    done
-			).crt"
+			pem="$(ossl_get_field4dn_by_name "$9" 'CN').crt"
 			if ! (V=0 valid_file "$pem"); then
 				return 0
 			fi